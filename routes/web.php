498ae52e74f05212bfb5a981bf48daff38d9a00d
Enzo Trujillo <enzot.13@gmail.com>
2025-03-13 00:51:16 -0300

    Update authentication and CORS settings; require password confirmation on registration, restrict allowed origins, and enable credentials support. Modify root route to return a JSON error response.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    return response()->json(['error' => 'Unauthorized'], 401);
 });
